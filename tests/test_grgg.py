35d3f84c3aa775616e3108d2654cfa1af39adf6d
Szymon Talaga <stalaga@protonmail.com>
2025-08-20 01:08:13 +0200

    Add some basic tests for complementarity.

--- tests/test_grgg.py
+++ tests/test_grgg.py
@@ -7,3 +7,9 @@ class TestSimilarityRGG:
     def test_kbar(self, model_sim: GRGG, kbar: float) -> None:
         model_sim.calibrate(kbar)
         assert model_sim.kbar == pytest.approx(kbar, rel=1e-2)
+
+
+class TestComplementarityRGG:
+    def test_kbar(self, model_comp: GRGG, kbar: float) -> None:
+        model_comp.calibrate(kbar)
+        assert model_comp.kbar == pytest.approx(kbar, rel=1e-2)
