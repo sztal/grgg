5e622d671737a40bb7fa1e958c999daf43c42cc4
Szymon Talaga <stalaga@protonmail.com>
2025-08-27 12:00:44 +0200

    Restructure project, so anything project-specific do not get included in the package distribution file.

--- grgg/project/__init__.py
+++ grgg/project/__init__.py
@@ -6,11 +6,15 @@ import igraph
 from dotenv import load_dotenv
 from omegaconf import OmegaConf
 
+from .plotting import theme
+
 load_dotenv()  # take environment variables from .env, if defined
 
 
 def make_paths(root: str | Path | None = None) -> SimpleNamespace:
-    root = Path(__file__).parent.parent if root is None else Path(root).absolute()
+    root = (
+        Path(__file__).parent.parent.parent if root is None else Path(root).absolute()
+    )
     paths = SimpleNamespace(
         root=root,
         figures=root / "figures",
